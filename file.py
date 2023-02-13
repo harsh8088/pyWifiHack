d39f7f8332101566cb4ca8f210f2e7d146980039
Harsh Bangari <harshrock2010@gmail.com>
2023-02-13 14:04:40 +0530

    initial commit

---
+++ file.py
@@ -0,0 +1,19 @@
+import itertools as its
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    words = "1234567890abcdefghijklmnopqrstuvwxyz"  # a set of password characters
+    r = its.product(words, repeat=8)  # random combination of 8 characters
+    dic = open("pwd.txt", "a")  # store wifi combinations in file
+    for i in r:
+        dic.write("".join(i))
+        dic.write("".join("\n"))
+    dic.close()
+    print('end..')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
