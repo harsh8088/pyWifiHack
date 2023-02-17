846fa1e8d692cb04f1b614df0a174a8c2df0c2a7
Harsh Bangari <harshrock2010@gmail.com>
2023-02-17 12:27:38 +0530

    updated

--- file.py
+++ file.py
@@ -12,8 +12,7 @@ def print_hi(name):
         dic.write("".join("\n"))
     dic.close()
     print('end..')  # Press ⌘F8 to toggle the breakpoint.
-
-
+    
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
