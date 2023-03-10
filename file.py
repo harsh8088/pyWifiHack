d39cb7426b61926dc2e01c350514cad32bbbe291
Harsh Bangari <harshrock2010@gmail.com>
2023-03-10 11:25:15 +0530

    updated

--- file.py
+++ file.py
@@ -1,6 +1,5 @@
 import itertools as its
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
